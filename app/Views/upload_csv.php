ae85981cdc6fd4f9ffb54345a9cdad5aa5c74251
juanmapf97 <juanmapf97@gmail.com>
2020-08-25 09:53:31 -0500

    Autocomplete off in form

--- app/Views/upload_csv.php
+++ app/Views/upload_csv.php
@@ -10,7 +10,7 @@
     <div class="w-full h-screen flex items-center justify-center">
         <div class="w-3/4 flex flex-col justify-center">
         <h1 class="text-6xl font-extrabold tracking-wide leading-snug">Check your domains <span class="text-blue-500">availability & authority</span> easily, through a csv</h1>
-            <form id="upload_form" action="/upload/do_upload" method="post" enctype="multipart/form-data">
+            <form id="upload_form" autocomplete="off" action="/upload/do_upload" method="post" enctype="multipart/form-data">
                 <div class="border border-dashed border-gray-500 relative">
                     <input type="file" accept=".csv" name="userfile" class="cursor-pointer relative block opacity-0 w-full h-full p-20 z-50">
                     <div class="text-center p-10 absolute top-0 right-0 left-0 m-auto">
